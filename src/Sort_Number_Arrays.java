431915557b5378488e7da7bbb9b5d9befacee4ff
Devin Smoot <smootd@student.swosu.edu>
2015-11-22 23:53:44 -0600

    100% stable code. Ready for merge to master branch

--- src/Sort_Number_Arrays.java
+++ src/Sort_Number_Arrays.java
@@ -1,44 +1,55 @@
-import java.util.*;
+/* Project:		Homework 10: Sorting 6 numbers
+ * Author:		Devin Smoot
+ * Date:		2015-11-18
+ */
 
+import java.util.Scanner;
+import java.util.Arrays;
 public class Sort_Number_Arrays {
 	public static void main(String[] args) {
-		System.out.println("Hello class, this is a sorting algorithm using arrays.");
+		//State program purpose
+		System.out.println("We will sort 6 numbers that you choose.");
 		
-		Scanner userInput = new Scanner(System.in);
+		System.out.println("\nEnter 6 numbers:");
 		
-		System.out.println("Enter 3 numbers:");
+		//Set up arrays to hold 6 integers
+		int[] numbers = new int[6];
+		//Call inputNumbers method
+		inputNumbers(numbers);
 		
-		System.out.print("Your first number:");
-		int n1 = userInput.nextInt();
-		
-		System.out.print("Your second number:");
-		int n2 = userInput.nextInt();
-		
-		System.out.print("Your third number:");
-		int n3 = userInput.nextInt();
-		
-		//Begin Arrays
-		
-		//Create a new array of 3 ints
-		int[] numbers = new int[3];
-		
-		/*
-		Arrays have an index that start with 0
-		and end with their length minus 1 such that
-		an array of 3 ints will have an index from 0-2
-		*/
-		
-		//Here we assign values to each index
-		numbers[0] = n1;
-		numbers[1] = n2;
-		numbers[2] = n3;
-		
-		//And sort them
+		//Sort the array using java utilities
 		Arrays.sort(numbers);
 		
+		//Call printArray method
+		printArray(numbers);
+		
+	}//End main method
+	public static void printArray (int numbers[]){
 		//And output them with a for loop
-		for(int i=0;i < numbers.length; i++){
-			System.out.println(numbers[i]);
-		}
-	}
+				System.out.print ("\nThe sorted array list: \n");
+				for(int i=0;i < numbers.length; i++){
+					System.out.println(numbers[i]);
+				}//End FOR
+	}//End printArray method
+	
+	public static void inputNumbers (int numbers[]){
+		//Define local variables
+		int userNumber;
+		//Set up scanner input
+		Scanner input = new Scanner(System.in);
+		//FOR loop to take inputs and place in the array
+		for (int i =0;i<6;i++){
+		System.out.print ("Input number "+ (i+1) + ": ");		
+		//Take input and validate that input is an integer
+			do {
+				while (!input.hasNextInt()) {
+		           System.out.println("That's not a number! Please enter a number.");
+		           input.next(); // this is important!
+		        }//End WHILE
+			 userNumber = input.nextInt(); // this is important!
+			}while (userNumber <= 0);//End DO
+		numbers[i] = userNumber;
+		}//End FOR
+	}//End inputNumbers method
+	
 }
